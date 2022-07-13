1b2ae203359faa61dc57cbb0e5d41224043d84c3
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2022-07-13 12:10:14 +0300

    simple pages added

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+        <header className="p-2 bg-dark text-light">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-dark d-flex justify-content-between">
+                    <ul className="navbar-nav d-flex align-items-baseline">
+                        <li className="nav-item">
+                            <a className="navbar-brand fw-bolder fs-4 mx-4" href="/">Income Calculator</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link mx-2" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link mx-2" href="/about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link mx-2" href="#">Help</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        );
+    }
+}
+
+export default Navbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./Navbar";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Navbar />
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
